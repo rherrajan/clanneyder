6bd8f626f2a7cde5e9fc86332eabab42cf36e98d
rherrajan <currant@gmx.de>
2019-04-22 11:41:03 +0200

    smaller classes

--- src/main/resources/archetype-resources/backend/src/test/java/DBExampleTest.java
+++ src/main/resources/archetype-resources/backend/src/test/java/DBExampleTest.java
@@ -4,26 +4,14 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-import javax.sql.DataSource;
-
-import org.junit.Before;
 import org.junit.Test;
 
-public class DBExampleTest {
-
-	private DataSource dataSource;
-
-	@Before
-	public void provideDatabase() throws Exception {
-		DBProvider dbProvider = new DBProvider();
-		dbProvider.dbUrl = System.getenv("JDBC_DATABASE_URL");
-		this.dataSource = dbProvider.dataSource();
-	}
+public class DBExampleTest extends AbstractDBTest {
 	
 	@Test
 	public void shouldReturnSomething() throws Exception {
 		DBExample dBExample = new DBExample();
-		dBExample.dataSource = dataSource;
+		dBExample.dataSource = getDataSource();
 		
 		Object response = dBExample.getDbexample();
 		assertThat(response, is(notNullValue()));
